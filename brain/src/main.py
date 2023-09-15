bdf10e88d61efcac41ab1bfeb6512c4c27b7f423
ewang184 <81916896+ewang184@users.noreply.github.com>
2023-09-15 16:55:43 -0400

    Update main.py

--- brain/src/main.py
+++ brain/src/main.py
@@ -5,7 +5,6 @@ import sys
 import os
 sys.path.insert(1, os.path.join(sys.path[0], '../lib'))
 
-from computer_vision import ComputerVision
 from navigation import Navigation
 from robot_client import RobotClient
 
@@ -13,7 +12,6 @@ flower_bot_ip = 'http://raspberrypi:5000'
 
 if __name__ == "__main__":
     flower_bot = RobotClient(flower_bot_ip)
-    flower_bot_cv = ComputerVision
     flower_bot_nav = Navigation(flower_bot, flower_bot_cv)
 
     #flower_bot_nav.drive_to_flower()
