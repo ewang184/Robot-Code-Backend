ebe08802e3eca8c8ae0ce20b1bd1b88551d91256
ewang184 <81916896+ewang184@users.noreply.github.com>
2023-09-15 16:53:49 -0400

    Update navigation.py

--- brain/src/navigation.py
+++ brain/src/navigation.py
@@ -10,20 +10,5 @@ class Navigation:
         if computer_vision is not None:
             self.computer_vision = computer_vision
 
-
-    def drive_to_flower(self):
-        # blink the led
-        if self.robot_client is not None:
-            while True:
-                self.robot_client.send_y()
-                self.robot_client.send_forward(12)
-                self.robot_client.send_forward(-12)
-                time.sleep(1)
-                self.robot_client.send_n()
-                self.robot_client.send_angle(11)
-                self.robot_client.send_angle(-11)
-                time.sleep(1)
-
-
     def start_keyboard_control(self):
         KeyboardControl(self.robot_client)
